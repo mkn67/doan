7ed749583196f1b47b9dcec529dba703751342ef
mkn67 <maikynguyen67@gmail.com>
2026-03-10 16:19:30 +0700

    Step 3: Upgrade Java Version to 21 - Compile: SUCCESS

---
+++ src/test/java/com/example/demo/ApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
